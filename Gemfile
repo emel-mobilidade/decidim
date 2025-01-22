a41b60dddc144716bd6304d02927ffd28561da95
Ivan Vergés <ivan@pokecode.net>
2025-01-22 17:04:45 +0100

    add twilio dep

--- Gemfile
+++ Gemfile
@@ -19,7 +19,7 @@ gem "decidim-term_customizer", github: "mainio/decidim-module-term_customizer", 
 gem "bootsnap", "~> 1.7"
 gem "health_check"
 gem "puma", ">= 6.3.1"
-#gem "twilio-ruby"
+gem "twilio-ruby"
 
 group :development, :test do
   gem "byebug", "~> 11.0", platform: :mri
--- app/services/TwilioSmsGatewayService.rb
+++ app/services/TwilioSmsGatewayService.rb
@@ -1,4 +1,4 @@
-class TwilioGatewayService
+class TwilioSmsGatewayService
   attr_reader :mobile_phone_number, :code, :context
   
   def initialize(mobile_phone_number, code, context = {})
@@ -6,9 +6,20 @@ class TwilioGatewayService
     @code = code
     @context = context
   end
+
+  def client
+    @client ||= Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
+  end
   
   def deliver_code
-    # Actual code to deliver the code
-    true
+    client.messages.create(
+      body: I18n.t('twilio_sms_gateway_service.deliver_code.body', code:, organization: organization_name),
+      from: ENV['TWILIO_PHONE_NUMBER'],
+      to: mobile_phone_number
+    )
+  end
+
+  def organization_name
+    context[:organization].name
   end
 end
